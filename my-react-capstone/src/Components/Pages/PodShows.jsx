0ad442451ab2546d6a498189396a145341184a49
teencube <tendaiincube22@gmail.com>
2024-04-10 16:48:18 +0200

    updates

--- my-react-capstone/src/Components/Pages/PodShows.jsx
+++ my-react-capstone/src/Components/Pages/PodShows.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import  { useState } from 'react';
 
 const PodShows = ({ show }) => {
   const [selectedSeasonId, setSelectedSeasonId] = useState(null);
--- my-react-capstone/src/PreviewCard.jsx
+++ my-react-capstone/src/PreviewCard.jsx
@@ -1,6 +1,6 @@
 
 
-import React, { useState } from 'react';
+import  { useState } from 'react';
 import FavoriteIcon from '@mui/icons-material/Favorite';
 import { Link } from 'react-router-dom';
 
